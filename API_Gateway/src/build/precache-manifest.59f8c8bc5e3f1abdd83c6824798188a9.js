53bc139fc2edeef16e7ab20fdee937444621b950
Jhamm <julianhamm1@gmail.com>
2020-04-08 00:49:49 +0100

    Updated website

---
+++ API_Gateway/src/build/precache-manifest.59f8c8bc5e3f1abdd83c6824798188a9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ffaf3634cd7c85a5360ecc290f1f0e4",
+    "url": "/static/media/pattern.2ffaf363.png"
+  },
+  {
+    "revision": "ff44c4a5c25a38242be1",
+    "url": "/static/css/main.87cb3050.chunk.css"
+  },
+  {
+    "revision": "fe84d07ea84364c961c271e3ea99f74a",
+    "url": "/static/media/parallax.fe84d07e.jpg"
+  },
+  {
+    "revision": "8d34711b957284624ab5",
+    "url": "/static/js/1.8d34711b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ff44c4a5c25a38242be1",
+    "url": "/static/js/main.ff44c4a5.chunk.js"
+  },
+  {
+    "revision": "e9a23f390263d5af13f102cd87597cef",
+    "url": "/static/media/bg-top.e9a23f39.png"
+  },
+  {
+    "revision": "b59a3d57307864aafcb0c77d30c8242e",
+    "url": "/static/media/bg-bottom.b59a3d57.png"
+  },
+  {
+    "revision": "edf8d788e75313194c5ad57338eb1ebd",
+    "url": "/static/media/pattern-2.edf8d788.png"
+  },
+  {
+    "revision": "8d34711b957284624ab5",
+    "url": "/static/css/1.323cb05c.chunk.css"
+  },
+  {
+    "revision": "77bba5059d511fb92f8437efbfa23361",
+    "url": "/index.html"
+  }
+];
